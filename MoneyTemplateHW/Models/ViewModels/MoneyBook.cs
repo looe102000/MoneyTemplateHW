e3524e4db5fbf887be19642bbcae72ed108d71d9
robert <looe102000@gmail.com>
2018-07-07 23:34:48 +0800

    View Model 建立完成

---
+++ MoneyTemplateHW/Models/ViewModels/MoneyBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoneyTemplateHW.Models.ViewModels
+{
+    /// <summary>
+    /// 記帳本 VIEW MODEL
+    /// </summary>
+    public class MoneyBook
+    {
+        /// <summary>
+        /// 類別
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        public string Type { get; set; }
+        /// <summary>
+        /// 日期
+        /// </summary>
+        /// <value>
+        /// The date.
+        /// </value>
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// 金額
+        /// </summary>
+        /// <value>
+        /// The amount.
+        /// </value>
+        public decimal Amount { get; set; }
+    }
+}
