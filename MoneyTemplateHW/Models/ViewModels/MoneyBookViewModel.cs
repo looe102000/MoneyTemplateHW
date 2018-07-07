bf1b025cfb49a1205f2d1f2df27ef81f1bd80898
robert <looe102000@gmail.com>
2018-07-08 02:32:52 +0800

    加入測資

---
+++ MoneyTemplateHW/Models/ViewModels/MoneyBookViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace MoneyTemplateHW.Models.ViewModels
+{
+    /// <summary>
+    /// 記帳本 VIEW MODEL
+    /// </summary>
+    public class MoneyBookViewModel
+    {
+        public class MBOOKDATA
+        {
+            /// <summary>
+            /// 類別
+            /// </summary>
+            /// <value>
+            /// The type.
+            /// </value>
+            public string category { get; set; }
+
+            /// <summary>
+            /// 日期
+            /// </summary>
+            /// <value>
+            /// The date.
+            /// </value>
+            public DateTime date { get; set; }
+
+            /// <summary>
+            /// 金額
+            /// </summary>
+            /// <value>
+            /// The amount.
+            /// </value>
+            public decimal money { get; set; }
+        }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
+        public string description { get; set; }
+
+        /// <summary>
+        /// The category select item list
+        /// </summary>
+        public List<SelectListItem> categorySelectItemList = new List<SelectListItem>
+            {
+                new SelectListItem{Text="請選擇",Value=null,Selected = true},
+                new SelectListItem{Text="支出",Value=null},
+                new SelectListItem{Text="收入",Value=null}
+            };
+
+        /// <summary>
+        /// The test
+        /// </summary>
+        public List<MBOOKDATA> test = new List<MBOOKDATA>();
+
+       
+        public MoneyBookViewModel()
+        {
+            var PATH = HostingEnvironment.MapPath("~");
+            var allTestData = XDocument.Load(PATH + "\\Models\\ViewModels\\TEST_DATA.xml").Elements("records").Elements("record");
+
+            foreach (var item in allTestData)
+            {
+                test.Add(new MBOOKDATA
+                {
+                    category = item.Element("category").Value
+                });
+            }
+        }
+
+       
+
+    }
+}
