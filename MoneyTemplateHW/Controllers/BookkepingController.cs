4d2ed029ca5a12ef1bfa696d17b1f411185cef3a
robert <looe102000@gmail.com>
2018-07-08 08:37:33 +0800

    日期大到小；金額大到小

--- MoneyTemplateHW/Controllers/BookkepingController.cs
+++ MoneyTemplateHW/Controllers/BookkepingController.cs
@@ -9,7 +9,10 @@ namespace MoneyTemplateHW.Controllers
         // GET: BookkepingController
         public ActionResult Index(MoneyBookViewModel MBV)
         {
-            MBV.MoneyBookData_List = MBV.MoneyBookData_List.AsEnumerable().OrderByDescending(x => x.date).ToList();
+            //日期大到小；金額大到小
+            MBV.MoneyBookData_List = MBV.MoneyBookData_List.AsEnumerable()
+                                                            .OrderByDescending(x => x.date)
+                                                            .ThenByDescending(x =>x.money).ToList();
 
             return View(MBV);
         }
