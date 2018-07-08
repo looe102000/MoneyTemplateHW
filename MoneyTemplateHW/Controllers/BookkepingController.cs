6469782b195374a8b601263a26c429375bcd35be
robert <looe102000@gmail.com>
2018-07-08 11:09:21 +0800

    新增分頁功能，一頁顯示 50

--- MoneyTemplateHW/Controllers/BookkepingController.cs
+++ MoneyTemplateHW/Controllers/BookkepingController.cs
@@ -1,4 +1,5 @@
 ﻿using MoneyTemplateHW.Models.ViewModels;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -7,12 +8,22 @@ namespace MoneyTemplateHW.Controllers
     public class BookkepingController : Controller
     {
         // GET: BookkepingController
-        public ActionResult Index(MoneyBookViewModel MBV)
+        public ActionResult Index(MoneyBookViewModel MBV, int? id)
         {
+            if (id == null)
+            {
+                id = 0;
+            }
+
+            var pageCnt = (int)id;
+            //分 10 頁
+            var pageRows = 50;
+
             //日期大到小；金額大到小
             MBV.MoneyBookData_List = MBV.MoneyBookData_List.AsEnumerable()
                                                             .OrderByDescending(x => x.date)
-                                                            .ThenByDescending(x =>x.money).ToList();
+                                                            .ThenByDescending(x =>x.money).Skip((pageCnt -1) * pageRows).Take(pageRows).ToList();
+
 
             return View(MBV);
         }
