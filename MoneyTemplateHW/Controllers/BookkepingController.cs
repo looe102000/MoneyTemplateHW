af2fc0342ef96e71f0632584dcb2049ac4619375
robert <looe102000@gmail.com>
2018-07-15 23:00:20 +0800

    完成 DB 寫入、HtmlHelper

--- MoneyTemplateHW/Controllers/BookkepingController.cs
+++ MoneyTemplateHW/Controllers/BookkepingController.cs
@@ -1,5 +1,7 @@
-﻿using MoneyTemplateHW.Models.ViewModels;
+﻿using MoneyTemplateHW.Models;
+using MoneyTemplateHW.Models.ViewModels;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -8,8 +10,29 @@ namespace MoneyTemplateHW.Controllers
     public class BookkepingController : Controller
     {
         // GET: BookkepingController
-        public ActionResult Index()
+        public ActionResult Index(MoneyBookViewModel MBV)
         {
+            if (string.IsNullOrWhiteSpace(MBV.Category) == false
+               && string.IsNullOrWhiteSpace(MBV.Date) == false
+               && string.IsNullOrWhiteSpace(MBV.Money.ToString()) == false
+               && string.IsNullOrWhiteSpace(MBV.Description) == false)
+            {
+                SkillTreeHomeworkEntities db = new SkillTreeHomeworkEntities();
+
+                db.AccountBook.Add(new AccountBook
+                {
+                    Id = Guid.NewGuid(),
+                    Categoryyy = Convert.ToInt32(MBV.Category),
+                    Amounttt = Convert.ToInt32(MBV.Money),
+                    Dateee = Convert.ToDateTime(MBV.Date),
+                    Remarkkk = MBV.Description
+                });
+
+                db.SaveChanges();
+            }
+
+            GetCategoryDropdownListModel();
+
             return View();
         }
 
@@ -36,6 +59,26 @@ namespace MoneyTemplateHW.Controllers
                                                             .ToList();
 
             return View();
+        }
+
+
+        private void GetCategoryDropdownListModel()
+        {
+            var options = new List<CategoryItem>
+            {
+                new CategoryItem() { name = "請選擇", value = null},
+                new CategoryItem() { name = "支出", value = 0},
+                new CategoryItem() { name = "收入", value = 1},
+            };
+
+            ViewData["CategoryListItem"] = new SelectList(options,  "value", "name", 0);
+        }
+
+
+        class CategoryItem
+        {
+            public string name { get; set; }
+            public int? value { get; set; }
         }
     }
 }
---
+++ MoneyTemplateHW/Helper/CategoryHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MoneyTemplateHW.Helper
+{
+    public static class CategoryHelper
+    {
+        /// <summary>
+        /// Displays the color of the category.
+        /// </summary>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="item">The item.</param>
+        /// <returns></returns>
+        public static HtmlString DisplayCategoryColor(this HtmlHelper htmlHelper, string item)
+        {
+            var className = "";
+            var itemName = "";
+
+            /*
+             * 類型的「支出」字樣顯現為紅色
+             * 類型的「收入」字樣顯現為藍色
+            */
+            switch (item)
+            {
+                case "1":
+                    className = "danger";
+                    itemName = "收入";
+                    break;
+
+                case "2":
+                    className = "primary";
+                    itemName = "支出";
+
+                    break;
+
+                case "0":
+                    className = "primary";
+                    itemName = "支出";
+
+                    break;
+            }
+
+            return new MvcHtmlString($"<span class='text-{className}'>{itemName}</span>");
+        }
+    }
+}
